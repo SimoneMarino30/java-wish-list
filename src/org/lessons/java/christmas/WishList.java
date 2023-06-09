0e52671209c286d39ab217d5e4c6c7e4eece6549
Simone Marino <simo.marino@outlook.it>
2023-06-09 15:46:23 +0200

    Added organisation collections sort

--- src/org/lessons/java/christmas/WishList.java
+++ src/org/lessons/java/christmas/WishList.java
@@ -1,6 +1,7 @@
 package org.lessons.java.christmas;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,29 +22,31 @@ public class WishList {
 
             // aggiungo il regalo alla wishlist
             wishList.add(present);
+            // ordino la lista alfabeticamente
+            Collections.sort(wishList);
+
             System.out.println("----------WISHLIST---------");
             System.out.println(wishList);
             System.out.println("---------------------------");
             // riepilogo temporaneo della lista
             System.out.println("You have " + wishList.size() + " presents in your wishlist");
 
-                // chiedo all' utente se vuole continuare
-                System.out.print("Would you like to add another present? (y/n)");
-                String answer = scan.nextLine().toLowerCase();
+            // chiedo all' utente se vuole continuare
+            System.out.print("Would you like to add another present? (y/n)");
+            String answer = scan.nextLine().toLowerCase();
 
-                if(answer.equals("y")) {
-                    System.out.println("Please add another present: ");
-                } else if(answer.equals("n")) {
-                    keepGoing = false;
-                    System.out.println("Here's your summary: ");
-                    for (String gift : wishList) {
-                        System.out.println(gift);
-                    }
-                    System.out.println("MERRY CHRISTMAS !!");
-                }else {
-                    System.out.println("Invalid input. Please enter 'y' to continue or 'n' to see your summary.");
+            if(answer.equals("y")) {
+                System.out.println("Please add another present: ");
+            } else if(answer.equals("n")) {
+                keepGoing = false;
+                System.out.println("Here's your summary: ");
+                for (String gift : wishList) {
+                    System.out.println(gift);
                 }
-
+                System.out.println("MERRY CHRISTMAS !!");
+            }else {
+                System.out.println("Invalid input. Please enter 'y' to continue or 'n' to see your summary.");
+            }
         }
 
     }
