7bc2c76319d1bce3e6fa920040461ddd41539273
Simone Marino <simo.marino@outlook.it>
2023-06-10 18:12:56 +0200

    Wishlist all working

--- src/org/lessons/java/christmas/WishList.java
+++ src/org/lessons/java/christmas/WishList.java
@@ -2,50 +2,69 @@ package org.lessons.java.christmas;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class WishList {
     public static void main(String[] args) {
         // istanzio la mia lista wishlist
-        List<String> wishList = new ArrayList<>();
-
-        // chiedo all' utente i regali da aggiungere alla lista
+        ArrayList<String> wishList = new ArrayList<>();
+        // Scanner input utente
         Scanner scan = new Scanner(System.in);
-
+        // chiedo all' utente i regali da aggiungere alla lista
+        System.out.println("What present would you like to add to your wishlist? ");
         // booleano per capire  se andare avanti ad aggiungere o stampare lista completa
         boolean keepGoing = true;
-        System.out.println("What present would you like to add to your wishlist? ");
+        // ciclo while
         while (keepGoing) {
-
             String present = scan.nextLine();
-
             // aggiungo il regalo alla wishlist
             wishList.add(present);
             // ordino la lista alfabeticamente
             Collections.sort(wishList);
-
+            // stampo la wishlist di riepilogo
             System.out.println("----------WISHLIST---------");
-            System.out.println(wishList);
+            for (String gift : wishList) {
+                System.out.println("-" + gift);
+            }
+            //System.out.println(wishList);
             System.out.println("---------------------------");
-            // riepilogo temporaneo della lista
-            System.out.println("You have " + wishList.size() + " presents in your wishlist");
+            // riepilogo dimensioni della lista
+
+            if(wishList.size() == 1 ) {
+                System.out.println("You have " + 1 + " present in your wishlist");
+            } else if (wishList.size() > 1) {
+                System.out.println("You have " + wishList.size() + " presents in your wishlist");
+            }
 
             // chiedo all' utente se vuole continuare
-            System.out.print("Would you like to add another present? (y/n)");
+            System.out.println("1-Would you like to add another present? (y/n)");
             String answer = scan.nextLine().toLowerCase();
+            /*System.out.print("Please add another present: ");*/
 
+            while(!answer.equals("y") && !answer.equals("n"))  {
+                // qualsiasi  altra risposta
+                System.out.println("-----------------");
+                System.out.println("INVALID INPUT \n" +
+                        "Please enter 'y' to continue or 'n' to see your summary.");
+                System.out.println("2-Would you like to add another present? (y/n)");
+                answer = scan.nextLine().toLowerCase();
+            }
             if(answer.equals("y")) {
-                System.out.println("Please add another present: ");
-            } else if(answer.equals("n")) {
+                // se la risposta è y
+                System.out.print("Please add another present: ");
+            }
+            if(answer.equals("n")) {
+                // se la risposta è n
                 keepGoing = false;
-                System.out.println("Here's your summary: ");
+                System.out.println("--------------------");
+                System.out.println("Here's your summary:");
+                System.out.println("--------------------");
                 for (String gift : wishList) {
-                    System.out.println(gift);
+                    System.out.println("-" + gift);
                 }
+                System.out.println("------------------");
                 System.out.println("MERRY CHRISTMAS !!");
-            }else {
-                System.out.println("Invalid input. Please enter 'y' to continue or 'n' to see your summary.");
+                System.out.println("------------------");
             }
         }
 
